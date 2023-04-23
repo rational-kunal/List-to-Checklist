fb475dd16cb147c5e828ad4d91183e844b264101
Kunal Kamble <kunalgk1999@gmail.com>
2023-04-23 14:44:22 +0530

    🎉 Generated the project by create-chrome-ext tool

---
+++ src/options/Options.jsx
@@ -0,0 +1,12 @@
+import './Options.css'
+
+function App() {
+  return (
+    <main>
+      <h3>Options Page!</h3>
+      <h6>v 0.0.0</h6>
+    </main>
+  )
+}
+
+export default App
---
+++ src/popup/Popup.jsx
@@ -0,0 +1,12 @@
+import './Popup.css'
+
+function App() {
+  return (
+    <main>
+      <h3>Popup Page!</h3>
+      <h6>v 0.0.0</h6>
+    </main>
+  )
+}
+
+export default App
