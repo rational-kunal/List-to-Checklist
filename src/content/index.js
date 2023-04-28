58f21668add4ff9f437631f225a1fce2522ef7cc
Kunal Kamble <kunalgk1999@gmail.com>
2023-04-28 20:53:26 +0530

    ⚡️ Populate checked list only when needed

--- src/content/index.js
+++ src/content/index.js
@@ -2,15 +2,15 @@ import { convert } from '../core/converter'
 import selector from '../core/selector'
 import settings from '../core/settings'
 
-// Populate the checked list
-await selector.populateCheckedList()
-
 // Convert List to Checklist
 // TODO: Execute this once per refresh
 const isExtensionEnable = await settings.isExtensionEnable()
 const isCurrentTabOptedIn = await settings.isCurrentTabOptedIn()
 
 if (isExtensionEnable && isCurrentTabOptedIn) {
+  // Populate the checked list
+  await selector.populateCheckedList()
+
   convert()
 }
 
