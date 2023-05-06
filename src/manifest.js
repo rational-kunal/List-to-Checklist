cb8226963f7c8124fbdd1809bba88b099c9d9243
Kunal Kamble <kunalgk1999@gmail.com>
2023-05-06 08:57:12 +0530

    🐛 Updated name of the extension

--- src/manifest.js
+++ src/manifest.js
@@ -1,7 +1,7 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
 export default defineManifest({
-  name: 'create-chrome-ext',
+  name: 'List to Checklist',
   description: 'A Chrome extension that converts all list elements into checklist elements.',
   version: '0.2.0',
   manifest_version: 3,
